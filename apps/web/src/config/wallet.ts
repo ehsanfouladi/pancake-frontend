97d624940c5960696d8aa7b383a7558611886d55
Ehsan Fouladi <ehsan.fouladi@gmail.com>
2023-07-12 14:36:10 +0330

    space again

--- apps/web/src/config/wallet.ts
+++ apps/web/src/config/wallet.ts
@@ -63,9 +63,9 @@ const walletsConfig = ({
         // && metaMaskConnector.ready
       },
       connectorId: ConnectorNames.MetaMask,
-      deepLink: 'https://metamask.app.link/dapp/apps3.cadinu.io/',
+      deepLink: 'https://metamask.app.link/dapp/cadinu.space/',
       qrCode,
-      downloadLink: 'https://metamask.app.link/dapp/apps3.cadinu.io/',
+      downloadLink: 'https://metamask.app.link/dapp/cadinu.space/',
     },
     {
       id: 'binance',
@@ -98,7 +98,7 @@ const walletsConfig = ({
       get installed() {
         return !!getTrustWalletProvider()
       },
-      deepLink: 'https://link.trustwallet.com/open_url?url=https://apps3.cadinu.io/',
+      deepLink: 'https://link.trustwallet.com/open_url?url=https://cadinu.space/',
       downloadLink: 'https://chrome.google.com/webstore/detail/trust-wallet/egjidjbpglichdcondbcbdnbeeppgdph',
       guide: {
         desktop: 'https://trustwallet.com/browser-extension',
