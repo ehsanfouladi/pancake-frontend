6b048c7479fa9692136a55d670c2cfd86279c2c7
Ehsan Fouladi <ehsan.fouladi@gmail.com>
2023-12-16 14:43:31 +0330

    eslint error fixed

--- apps/web/src/pages/migration.tsx
+++ apps/web/src/pages/migration.tsx
@@ -1,6 +1,9 @@
 import Migration from '../views/Migration'
+
 const MigrationPage = () => <Migration />
+
 export default MigrationPage
+
 // import { NotFound } from '@pancakeswap/uikit'
 
 // const NotFoundPage = () => <NotFound />
